7fada9e0a53080af202183775803696c710ec86e
Khushilal <singh.khushilal1119@gmail.com>
2023-12-31 22:45:12 +0545

    prepare_callback_added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,10 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, created_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
+from cnnClassifier.entity.config_entity import (
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+    PrepareCallbacksConfig,
+)
 
 
 class ConfigurationManager:
@@ -30,8 +34,7 @@ class ConfigurationManager:
         )
         return data_ingestion_config
 
-
-#for traaining od the model
+    # for traaining od the model
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
 
@@ -46,6 +49,38 @@ class ConfigurationManager:
             params_include_top=self.params.INCLUDE_TOP,
             params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES,
-            
         )
         return prepare_base_model_config
+
+    # for callback function
+
+
+class ConfigurationCallbacksManager:
+    def __init__(
+        self,
+        config_filepth=CONFIG_FILE_PATH,
+        params_filepath=PARAMS_FILE_PATH,
+    ):
+        self.config = read_yaml(config_filepth)
+
+        self.params = read_yaml(params_filepath)
+
+        created_directories([self.config.artifacts_root])
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+
+        # creating dir
+        created_directories(
+            [Path(model_ckpt_dir), Path(config.tensorboard_root_log_dir)]
+        )
+
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+        )
+
+        return prepare_callbacks_config
